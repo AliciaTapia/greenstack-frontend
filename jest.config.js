7ee98f870203d819514fcaba0b4ee02e7f0d46b9
AliciaTapia <alicia.tapia@gmail.com>
2025-03-26 21:55:38 -0500

    removing test config from package.json

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,30 @@
+
+// jest.config.js
 module.exports = {
     transform: {
-        '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Add this
     },
     moduleFileExtensions: ['js', 'jsx'],
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     reporters: [
-        'default',
-        ['jest-junit', { outputDirectory: 'test-results' }]
-      ]
-};
+      'default',
+      ['jest-junit', { outputDirectory: 'test-results' }]
+    ]
+  };
+  
+// module.exports = {
+//     transform: {
+//         '^.+\\.jsx?$': 'babel-jest',
+//     },
+//     moduleFileExtensions: ['js', 'jsx'],
+//     testEnvironment: 'jsdom',
+//     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+//     reporters: [
+//         'default',
+//         ['jest-junit', { outputDirectory: 'test-results' }]
+//       ]
+// };
